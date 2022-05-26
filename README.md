2c46485633dee126a71249abbf8b4a3747f6a84d
Mark Maksi <artistoflife74@gmail.com>
2022-05-26 13:12:09 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# picture-in-picture
+## Demo ⚡
+[https://markmaksi74.github.io/picture-in-picture/](https://markmaksi74.github.io/picture-in-picture/)
+
+## Features 🥁
+- Infinite Scrolling
+- Screen Capture API [https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture](https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture
+) which is one of Web APIs along with `DOM` and `fetch()`
+- Picture-in-Picture Web API [https://css-tricks.com/an-introduction-to-the-picture-in-picture-web-api/#aa-entering-picture-in-picture-mode](https://css-tricks.com/an-introduction-to-the-picture-in-picture-web-api/#aa-entering-picture-in-picture-mode)
---
+++ script.js
@@ -0,0 +1,29 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+const buttonShare = document.getElementById('button-share');
+
+
+// Prompt to select media stream, pass to video element, then play
+buttonShare.addEventListener('click', async () => {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Here
+  }
+})
+
+button.addEventListener('click', async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
